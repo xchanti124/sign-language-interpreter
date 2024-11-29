2e2f3e929483b53a92cd7168e0cf5d2754c2ac05
Chantal Ariu <chantalariu227@gmail.com>
2024-11-29 11:48:46 +0100

    Save letter from pressed key with associated camera input landmarks

--- mediapipe_hands.py
+++ mediapipe_hands.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+from input_handler import *
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -8,7 +9,7 @@ mp_hands = mp.solutions.hands
 # For static images:
 IMAGE_FILES = []
 with mp_hands.Hands(
-    static_image_mode=True, max_num_hands=2, min_detection_confidence=0.5
+    static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5
 ) as hands:
     for idx, file in enumerate(IMAGE_FILES):
         # Read an image, flip it around y-axis for correct handedness output (see
@@ -48,10 +49,11 @@ with mp_hands.Hands(
                 hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5
             )
 
-# For webcam input:
+# For video feed
+landmark_dict = create_landmark_coordinate_dict()
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
-    model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5
+    model_complexity=1, min_detection_confidence=0.5, min_tracking_confidence=0.5
 ) as hands:
     while cap.isOpened():
         success, image = cap.read()
@@ -69,17 +71,53 @@ with mp_hands.Hands(
         # Draw the hand annotations on the image.
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        custom_landmark_style = mp_drawing.DrawingSpec(
+            color=(0, 255, 0), thickness=2, circle_radius=2
+        )  # Green landmarks
+        custom_connection_style = mp_drawing.DrawingSpec(
+            color=(255, 0, 0), thickness=2
+        )  # Blue connections
+
+        x = 0
         if results.multi_hand_landmarks:
+            key_pressed = is_key_pressed()
+            itr = 0
+
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
                     image,
                     hand_landmarks,
                     mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style(),
+                    custom_landmark_style,
+                    custom_connection_style,
                 )
+
+                if key_pressed != -1:
+                    for i in range(0, 21):
+                        xy_tuple = (
+                            hand_landmarks.landmark[i].x,
+                            hand_landmarks.landmark[i].y,
+                        )
+                        landmark_dict[chr(key_pressed)].append(xy_tuple)
+                    print(landmark_dict)
+
         # Flip the image horizontally for a selfie-view display.
-        cv2.imshow("MediaPipe Hands", cv2.flip(image, 1))
+        image = cv2.flip(image, 1)
+
+        cv2.putText(
+            image,
+            "Wrist x:" + str(x),
+            (10, 80),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.7,
+            (255, 255, 255),
+            1,
+            cv2.LINE_AA,
+        )
+        cv2.imshow("meow meow meow meow", image)
+
         if cv2.waitKey(5) & 0xFF == 27:
             break
+
 cap.release()
