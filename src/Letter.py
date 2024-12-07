4b7601c0d1dc62a8f30ebec1fe5a7b0547a1fd86
alexvhz <alexvictor1305@gmail.com>
2024-12-07 21:48:27 +0100

    Rename load and save functions

--- src/Letter.py
+++ src/Letter.py
@@ -1,5 +1,4 @@
 import json
-import numpy as np
 
 def load_json(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
@@ -9,14 +8,14 @@ def save_json(file_path, data):
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
-def load_data(self):
+def load_json_data(self):
     try:
         self.coordinate_list = load_json('coordinate_data.json')
         self.label_list = load_json('label_data.json')
     except Exception:
         pass
 
-def save_data(self):
+def save_json_data(self):
     save_json('coordinate_data.json', self.coordinate_list)
     save_json('label_data.json', self.label_list)
 
@@ -26,7 +25,7 @@ class Letter:
     label_list = []
 
     def __init__(self, letter: chr, landmarks):
-        load_data(self)
+        load_json_data(self)
         self.letter = chr(letter)
         self.landmarks = landmarks
         self.process()
@@ -42,5 +41,5 @@ class Letter:
                 landmark_list.append(y)
 
         self.coordinate_list.append(landmark_list)
-        save_data(self)
+        save_json_data(self)
 
