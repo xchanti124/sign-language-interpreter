8d053aa236366ee36e314b1edc0d2097c4041a13
alexvhz <alexvictor1305@gmail.com>
2024-12-09 21:38:36 +0100

    Refactor code

--- src/Main.py
+++ src/Main.py
@@ -1,105 +1,37 @@
-import mediapipe as mp
+import cv2
 
-from src.Letter import Letter
-from src.InputHandler import *
+from src import InputHandler
+from src.InputHandler import updated_pressed_key
+from src.FrameProcessor import processFrame
+from src.State import State
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
+ESC_KEY = 27
+running = True
 
-# For static images:
-IMAGE_FILES = []
-with mp_hands.Hands(
-    static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5
-) as hands:
-    for idx, file in enumerate(IMAGE_FILES):
-        # Read an image, flip it around y-axis for correct handedness output (see
-        # above).
-        image = cv2.flip(cv2.imread(file), 1)
-        # Convert the BGR image to RGB before processing.
-        results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-        # Print handedness and draw hand landmarks on the image.
-        print("Handedness:", results.multi_handedness)
-        if not results.multi_hand_landmarks:
-            continue
-        image_height, image_width, _ = image.shape
-        annotated_image = image.copy()
-        for hand_landmarks in results.multi_hand_landmarks:
-            print("hand_landmarks:", hand_landmarks)
-            print(
-                f"Index finger tip coordinates: (",
-                f"{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, "
-                f"{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})",
-            )
-            mp_drawing.draw_landmarks(
-                annotated_image,
-                hand_landmarks,
-                mp_hands.HAND_CONNECTIONS,
-                mp_drawing_styles.get_default_hand_landmarks_style(),
-                mp_drawing_styles.get_default_hand_connections_style(),
-            )
-        cv2.imwrite(
-            "/tmp/annotated_image" + str(idx) + ".png", cv2.flip(annotated_image, 1)
-        )
-        # Draw hand world landmarks.
-        if not results.multi_hand_world_landmarks:
-            continue
-        for hand_world_landmarks in results.multi_hand_world_landmarks:
-            mp_drawing.plot_landmarks(
-                hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5
-            )
-
-# For video feed
 cap = cv2.VideoCapture(0)
-with mp_hands.Hands(
-    model_complexity=1, min_detection_confidence=0.5, min_tracking_confidence=0.5
-) as hands:
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            print("Ignoring empty camera frame.")
-            # If loading a video, use 'break' instead of 'continue'.
-            continue
 
-        # To improve performance, optionally mark the image as not writeable to
-        # pass by reference.
-        image.flags.writeable = False
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = hands.process(image)
+while running:
+    success, image = cap.read()
 
-        # Draw the hand annotations on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if not success:
+        print("Failed to grab frame")
+        continue
 
-        custom_landmark_style = mp_drawing.DrawingSpec(
-            color=(0, 255, 0), thickness=2, circle_radius=2
-        )  # Green landmarks
-        custom_connection_style = mp_drawing.DrawingSpec(
-            color=(255, 0, 0), thickness=2
-        )  # Blue connections
+    updated_pressed_key()
 
-        if results.multi_hand_landmarks:
-            key_pressed = is_key_pressed()
+    # More processing before showing the image
+    ########################################################################################
+    if InputHandler.current_state == State.SELECTING_LETTER:
+        pass
+    else:
+        image = processFrame(cap)
 
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    image,
-                    hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS,
-                    custom_landmark_style,
-                    custom_connection_style,
-                )
+    ########################################################################################
 
-                if key_pressed != -1:
-                    Letter(key_pressed, hand_landmarks)
+    cv2.imshow("meow meow meow meow", image)
 
-        # Flip the image horizontally for a selfie-view display.
-        image = cv2.flip(image, 1)
-
-        cv2.imshow("meow meow meow meow", image)
-
-        if cv2.waitKey(5) & 0xFF == 27:
-            break
+    if cv2.waitKey(1) & 0xFF == ESC_KEY:
+        running = False
 
 cap.release()
+cv2.destroyAllWindows()
