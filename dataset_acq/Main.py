ae18cdd29058fc55c4a762e413e24f68f1a5bafd
alexvhz <alexvictor1305@gmail.com>
2024-12-11 17:48:44 +0100

    Implement VideoCaptureThread and refactor code

--- dataset_acq/Main.py
+++ dataset_acq/Main.py
@@ -1,49 +1,57 @@
 import cv2
-
-from dataset_acq import InputHandler
-from dataset_acq.InputHandler import updated_pressed_key
-from dataset_acq.FrameProcessor import processFrame
+from dataset_acq import InputHandler, FrameProcessor
+from dataset_acq.InputHandler import updatedPressedKey
+from dataset_acq.FrameProcessor import processFrame, getHandLandmarks
 from dataset_acq.State import State
+from dataset_acq.VideoCaptureThread import VideoCaptureThread
 
-ESC_KEY = 27
-running = True
-count = 0
+def main():
+    cap = VideoCaptureThread(0)
+    ESC_KEY = 27
+    running = True
+    count = 0
 
-cap = cv2.VideoCapture(0)
+    while running:
+        image = cap.read()
+        if image is None:
+            print("No frame captured")
+            continue
 
-while running:
-    success, image = cap.read()
+        updatedPressedKey()
 
-    if not success:
-        print("Failed to grab frame")
-        continue
+        # More processing before showing the image
+        ########################################################################################
+        results = FrameProcessor.hands.process(image)
 
-    updated_pressed_key()
+        if InputHandler.current_state == State.SELECTING_LETTER:
+            text = "Press a letter to start capturing landmarks"
+            image = processFrame(cap.read(), False, results)
 
-    # More processing before showing the image
-    ########################################################################################
-    if InputHandler.current_state == State.SELECTING_LETTER:
-        text = "Press a letter to start capturing landmarks"
-        image = processFrame(cap, False)
+            getHandLandmarks(results)
 
-    else:
-        text = "Currently capturing landmarks for the letter " + chr(InputHandler.current_letter)
+        else:
+            text = "Currently capturing landmarks for the letter " + chr(InputHandler.current_letter)
 
-        image = processFrame(cap, True)
-        cv2.putText(image, "Press BACKSPACE to choose another letter or ESC to exit", (50, 100),  cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-        cv2.putText(image, "Samples: " + str(count), (50, 150), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2,  cv2.LINE_AA)
-        count += 1
-        if count == 1000:
-            InputHandler.current_state = State.SELECTING_LETTER
-            count = 0
+            image = processFrame(cap.read(), True, results)
+            cv2.putText(image, "Press BACKSPACE to choose another letter or ESC to exit", (50, 100),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(image, "Samples: " + str(count), (50, 150), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2,
+                        cv2.LINE_AA)
+            count += 1
+            if count == 1000:
+                InputHandler.current_state = State.SELECTING_LETTER
+                count = 0
 
-    cv2.putText(image, text, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-    ########################################################################################
+        cv2.putText(image, text, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        ########################################################################################
 
-    cv2.imshow("meow meow meow meow", image)
+        cv2.imshow("mrreeeowwwww", image)
 
-    if cv2.waitKey(1) & 0xFF == ESC_KEY:
-        running = False
+        if cv2.waitKey(1) & 0xFF == ESC_KEY:
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.stop()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
