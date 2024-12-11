5d9ad42b8b46c49a6857f75b926c23d6fe927a06
alexvhz <alexvictor1305@gmail.com>
2024-12-11 17:44:32 +0100

    Cache results for better performance

--- dataset_acq/FrameProcessor.py
+++ dataset_acq/FrameProcessor.py
@@ -19,22 +19,7 @@ custom_connection_style = mp_drawing.DrawingSpec(
     color=(255, 0, 0), thickness=2
 )
 
-def processFrame(cap, save_landmarks):
-    success, image = cap.read()
-    if not success:
-        print("Ignoring empty camera frame.")
-        # If loading a video, use 'break' instead of 'continue'.
-        return None
-
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
-
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+def processFrame(image, save_landmarks, results):
 
     if results.multi_hand_landmarks:
 
@@ -53,13 +38,7 @@ def processFrame(cap, save_landmarks):
 
     return image
 
-def getHandLandmarks(cap):
-    success, image = cap.read()
-    if not success:
-        print("Ignoring empty camera frame.")
-        return None
-
-    results = hands.process(image)
+def getHandLandmarks(results):
     landmark_list = []
 
     if results.multi_hand_landmarks:
@@ -70,4 +49,3 @@ def getHandLandmarks(cap):
             landmark_list.append(x)
             landmark_list.append(y)
 
-    return landmark_list
