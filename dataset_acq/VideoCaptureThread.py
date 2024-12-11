cafff710dc65c758a570c63ff1eb7af6b102b23e
alexvhz <alexvictor1305@gmail.com>
2024-12-11 17:43:48 +0100

    Create VideoCaptureThread class

---
+++ dataset_acq/VideoCaptureThread.py
@@ -0,0 +1,33 @@
+import cv2
+import threading
+
+class VideoCaptureThread:
+    def __init__(self, src=0):
+        self.cap = cv2.VideoCapture(src)
+        if not self.cap.isOpened():
+            raise ValueError(f"Unable to open video source {src}")
+        self.frame = None
+        self.running = True
+        self.lock = threading.Lock()
+
+        # Start the thread
+        self.thread = threading.Thread(target=self.update, daemon=True)
+        self.thread.start()
+
+    def update(self):
+        while self.running:
+            success, frame = self.cap.read()
+            if success:
+                with self.lock:
+                    self.frame = frame
+            else:
+                print("Failed to capture frame")
+
+    def read(self):
+        with self.lock:
+            return self.frame
+
+    def stop(self):
+        self.running = False
+        self.thread.join()
+        self.cap.release()
